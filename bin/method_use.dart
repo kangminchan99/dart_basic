4db9be10a1200a5c4901d13117c59de2a689b1d1
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-02-06 17:52:09 +0900

    Create method_use.dart

---
+++ bin/method_use.dart
@@ -0,0 +1,10 @@
+// Phone이라는 클래스 안에 getNetwork라는 메소드를 불러오는 법
+class Phone {
+  void getNetwork() {}
+}
+
+// phone이라는 인스턴스를 만들어줘야 한다.
+void main(List<String> args) {
+  Phone phone = Phone();
+  phone.getNetwork();
+}
