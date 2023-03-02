2da9a159eef99a9636eec683b31601cf6a739786
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-02 21:56:00 +0900

    Create collection&generic.dart

---
+++ bin/collection&generic.dart
@@ -0,0 +1,20 @@
+// int addNum(int num1, int num2) {
+//   return num1 + num2;
+// }
+
+// void main(List<String> args) {
+//   List<dynamic> number = [];
+
+//   number.add(2);
+//   number.add('test');
+//   number.add(7.4);
+//   number.add(addNum(3, 4));
+//   number.add(true);
+//   print(number);
+// }
+
+void main(List<String> args) {
+  List<String> names = [];
+  names.addAll(['Kang', 'Min', 'Chan']);
+  print(names);
+}
